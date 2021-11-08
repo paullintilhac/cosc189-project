added dr param to attack_wrapper so that it triggers correct gradient transform in attack

lib/attacks/nn_attacks.py
@@ -95,9 +95,8 @@ def fg(model_dict, data_dict, x_curr, y_curr, x_curr_orig, adv_x, dev_mag, b_c,
     rev = model_dict['rev']
     # Gradient w.r.t to input and current class
     delta_x = gradient(x_curr, y_curr)
-
     if dr_alg is not None:
-        print("doing gradient transform")
+        #print("doing gradient transform")
         A = gradient_transform(model_dict, dr_alg)
         delta_x = np.dot(delta_x.reshape(batch_len, no_of_features), A)
 
@@ -237,6 +236,7 @@ def attack_wrapper(model_dict, data_dict, input_var, target_var, test_prediction
     test_acc = acc_fn(test_prediction, target_var)
     validator = val_fn(input_var, target_var, test_loss, test_acc)
     print("dev_list: " + str(dev_list))
+    print("dr_alg in attack wrapper: " + str(dr_alg))
     o_list = []
     mag_count = 0
     for dev_mag in dev_list: